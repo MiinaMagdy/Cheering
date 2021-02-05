0cfd067304448508f6dfe80d0bcc4732acdcdc16
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2021-02-05 10:15:22 +0200

    Update cheer.py

--- cheer.py
+++ cheer.py
@@ -1,5 +1,10 @@
+# using this library (module) called "pyttsx3"
 import pyttsx3
+# prompt the user to input his name
 name = input("What's your name? ")
+# init the variable
 engine = pyttsx3.init()
+# order to say something
 engine.say(f"hello, {name}")
+# run the order and wait until the end of the voice
 engine.runAndWait()
